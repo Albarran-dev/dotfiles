4e7656922db737f86ca8980cce5f6b7da42a7119
Albarran <albarran9696@gmail.com>
2022-12-12 09:59:46 +0100

    Add .zshrc, ,oh-my-zsh files

--- brew.sh
+++ brew.sh
@@ -7,20 +7,28 @@
 brew update
 brew upgrade
 
+## Terminal
+brew install git
+brew install --cask iterm2
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+brew install zsh-syntax-highlighting
+sudo apt install colorls
+brew install --cask docker
+brew install docker
+brew install mysql
+
 ## Basic Apps and editors
 brew install --cask brave-browser
 brew install --cask visual-studio-code
 brew install --cask sublime-text
 brew install --cask mark-text
-brew install --cask docker
-brew install docker
-brew install mysql
 brew install --cask adobe-acrobat-pro
 brew install --cask cryptomator
-brew install --cask iterm2
 brew install --cask postman
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 ## Productivity Apps
 brew install --cask maccy
